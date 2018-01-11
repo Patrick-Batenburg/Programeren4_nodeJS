c708c10d3f911fef1aeab4e3be01a73279a033ae
Patrick <zammypatrick@gmail.com>
2018-01-11 21:37:26 +0100

    improved db and controllers

--- NodejsWebAppServer/api/status.api.js
+++ NodejsWebAppServer/api/status.api.js
@@ -3,7 +3,7 @@
 //
 var express = require('express');
 var routes = express.Router();
-var todoController = require('../controllers/todo.controller');
+var statusController = require('../controllers/status.controller');
 
 module.exports = {}
 
@@ -11,9 +11,6 @@ module.exports = {}
 // andere benadering dan je tot nu toe zag - routes zijn gescheiden van de controllers;
 // de controllers verzorgen de afhandeling. 
 //
-routes.get('/todos', todoController.getAll);
-routes.get('/todos/:id', todoController.getOneById);
-routes.get('/todos/errordemo', todoController.errorDemo);
-routes.all('/todos*', todoController.catchAll);
+routes.get('/status*', statusController.getStatus);
 
 module.exports = routes;
--- NodejsWebAppServer/controllers/todo.controller.js
+++ NodejsWebAppServer/controllers/todo.controller.js
@@ -1,4 +1,4 @@
-var db = require('../config/db');
+var db = require('../config/db.improved');
 
 module.exports = {
 
@@ -21,6 +21,24 @@ module.exports = {
     getOneById(req, res, next) {
         const id = req.params.id;
         db.query('SELECT * FROM todos WHERE ID=' + id, function (error, rows, fields) {
+            if (error) {
+                next(error);
+            } else {
+                res.status(200).json({
+                    status: {
+                        query: 'OK'
+                    },
+                    result: rows
+                }).end();
+            };
+        });
+    },
+
+    // Deze handler laat zien hoe een error via next door de errorhandler 
+    // in server.js wordt afgehandeld. 
+    errorDemo(req, res, next) {
+        console.log('todo.controller errorDemo');
+        db.query('SELECT * FROM nonExistentTable', function (error, rows, fields) {
             if (error) {
                 next(error);
             } else {
